73f311a308be39ce606c1e8b4380820a65de1922
Sintaa <sintawahyuni0911@gmail.com>
2021-06-09 18:22:55 -0700

    fragment

---
+++ app/src/main/java/com/example/sintafragment/ViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.sintafragment
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.sintafragment.FirstFragment
+import com.example.sintafragment.SecondFragment
+import kotlin.contracts.Returns
+
+class ViewPagerAdapter(private val mContext: Context, fm:FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return if (position == 0) {
+            FirstFragment.newInstance()
+        } else {
+            SecondFragment.newInstance()
+        }
+    }
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+    }
+    override fun getCount(): Int {
+        return 2
+    }
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.tab_text_1,
+            R.string.tab_text_2)
+
+    }
+    }
